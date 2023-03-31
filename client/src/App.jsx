2e0a0217cb2bed08bda9feeb2ffe1bc20f310758
unknown <cfermilan@ssct.edu.ph>
2023-03-31 14:41:32 +0800

    fontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import MainLayout from './components/MainLayout'
+import Home from './pages/Home'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route/>
+      <Route/>
+    </Route>
+  ))
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+      <ToastContainer/>
+    </>
   )
 }
 
