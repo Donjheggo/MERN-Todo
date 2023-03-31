676a0391d7559f321eaefff2ceecaeec0039d6d8
unknown <cfermilan@ssct.edu.ph>
2023-03-31 13:32:43 +0800

    fixed errorHandler

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode || 500
     res.status(statusCode).res.json({
         message: err.message,
-        stach: process.env.NODE_ENV === 'developement' ? err.stack : null
+        stach: process.env.NODE_ENV === 'development' ? err.stack : null
     });
 }
 
